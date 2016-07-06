7c1bd3b1b34d7ccf927760aaceaac572149b4f10
Sam Noedel <sam.noedel@gmail.com>
2016-07-05 22:32:00 -0700

    Add creation form

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,13 +4,15 @@ import { NgFor } from '@angular/common';
 import { Chore } from './chore';
 import { ChoreStoreService } from './stores/chore-store.service';
 import { ChoreListItemComponent } from './chore-list-item.component';
+import { ChoreEditComponent } from './chore-edit.component';
 import { ChoreActions } from './actions/chore-actions';
 
 @Component({
   selector: 'chore-app',
   template: `<header>Todo</header>
-    <chore-list-item *ngFor="let chore of chores" [chore]="chore"></chore-list-item>`,
-  directives: [ NgFor, ChoreListItemComponent ]
+    <chore-list-item *ngFor="let chore of chores" [chore]="chore"></chore-list-item>
+    <chore-edit></chore-edit>`,
+  directives: [ NgFor, ChoreListItemComponent, ChoreEditComponent ]
 })
 export class AppComponent implements OnInit {
   public chores: Array<Chore>;
@@ -20,7 +22,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.choreActions.create('Watch TV');
     this.chores = this.choreStore.getState();
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
+import { ChoreEditComponent } from './chore-edit.component';
 import { ChoreStoreService } from './stores/chore-store.service';
 import { FluxDispatcher } from './flux-dispatcher.service';
 import { ChoreActions } from './actions/chore-actions';
